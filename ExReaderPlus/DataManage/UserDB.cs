72a986cfe08cf63337c1985457afeaa5229f72c3
1196752479 <1196752479@qq.com>
2018-07-17 17:52:19 +0800

    增加单元测试

---
+++ ExReaderPlus/DataManage/UserDB.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExReaderPlus.DataManage
+{
+    /// <summary>
+    /// 创建用户的词库，可以为空，也可以导入
+    /// </summary>
+    public class UserDB
+    {
+        
+    }
+
+    /// <summary>
+    /// 用户信息
+    /// </summary>
+    public class User
+    {
+        private string userName;
+        private string password;
+
+        public string UserName
+        {
+            get;
+            set;
+        }
+
+        public string Password
+        {
+            get;
+            set;
+        }
+    }
+}
