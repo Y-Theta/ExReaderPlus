c9624b529a54080a8f7e9bf2167a1a63bc63d328
Y _Theta <2483507105@qq.com>
2018-07-17 15:53:38 +0800

    *RichTextBox -Render -PreRender

--- ExReaderPlus/CustomeControls/NewControls/RichTextBox.cs
+++ ExReaderPlus/CustomeControls/NewControls/RichTextBox.cs
@@ -16,7 +16,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Documents;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-
+using Windows.UI.Text;
+using Windows.UI;
 
 namespace ExReaderPlus.CustomeControls.NewControls {
     /// <summary>
@@ -30,6 +31,11 @@ namespace ExReaderPlus.CustomeControls.NewControls {
         /// 计时器，用于在文本变化后一定时间内刷新字典
         /// </summary>
         private Timer _refreshdic;
+
+        /// <summary>
+        /// 内容文本，节省每次预渲染时的开销
+        /// </summary>
+        private string _contentString;
 
         /// <summary>
         /// 转换完成标志,若不为0表示文本有变动
@@ -62,7 +68,23 @@ namespace ExReaderPlus.CustomeControls.NewControls {
         }
         public static readonly DependencyProperty KeywordsProperty =
             DependencyProperty.Register("Keywords", typeof(List<Rendergroup>),
-                typeof(RichTextBox), new PropertyMetadata(null));
+                typeof(RichTextBox), new PropertyMetadata(null, KeywordsChanged));
+        private static void KeywordsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+
+        }
+
+        /// <summary>
+        /// 文字预渲染，默认关闭
+        /// </summary>
+        public bool TextPrerender {
+            get { return (bool)GetValue(TextPrerenderProperty); }
+            set { SetValue(TextPrerenderProperty, value); }
+        }
+        public static readonly DependencyProperty TextPrerenderProperty =
+            DependencyProperty.Register("TextPrerender", typeof(bool),
+                typeof(RichTextBox), new PropertyMetadata(false));
+
+
         #endregion
 
 
@@ -84,13 +106,17 @@ namespace ExReaderPlus.CustomeControls.NewControls {
             base.OnApplyTemplate();
         }
 
+        protected override void OnPointerPressed(PointerRoutedEventArgs e) {
+            base.OnPointerPressed(e);
+            var s = GetPointedWord(e.GetCurrentPoint(this).Position);
+            Debug.WriteLine(s);
+        }
+
         private void RichTextBox_Paste(object sender, TextControlPasteEventArgs e) {
-            
 
         }
 
         private void RichTextBox_TextChanged(object sender, RoutedEventArgs e) {
-            //TODO: 在此处理将输入转为单词字典
             TransformComplete = false;
         }
         #endregion
@@ -107,10 +133,13 @@ namespace ExReaderPlus.CustomeControls.NewControls {
         }
 
         private void _refreshdic_Elapsed(object sender, ElapsedEventArgs e) {
-            if (!_transformComplete) {
+            if (!_transformComplete)
+            {
                 UpdateDic();
                 _transformComplete = true;
-            } else {
+            }
+            else
+            {
                 _refreshdic.Enabled = false;
                 ElementSorted?.Invoke(this, null);
             }
@@ -122,11 +151,13 @@ namespace ExReaderPlus.CustomeControls.NewControls {
         private void UpdateDic() {
             string s = "";
             ElementsLoc.Clear();
-            this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => {
+            this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
                 Document.GetText(Windows.UI.Text.TextGetOptions.None, out s);
             }).AsTask().Wait();
             MatchCollection mc = Regex.Matches(s, @"[a-zA-Z]+");
-            foreach (Match m in mc) {
+            foreach (Match m in mc)
+            {
                 AddtoLocDic(m.Value, new Range(m.Index, m.Index + m.Length));
             }
 
@@ -137,24 +168,59 @@ namespace ExReaderPlus.CustomeControls.NewControls {
         /// <summary>
         /// 由于文章中会有重复的单词，所以用这种方式添加到字典
         /// </summary>
-        /// <param name="key"></param>
-        /// <param name="value"></param>
         private void AddtoLocDic(string key, Range value) {
-            if (ElementsLoc.ContainsKey(key)) {
+            if (ElementsLoc.ContainsKey(key))
+            {
                 ElementsLoc[key].Add(value);
-            } else {
+            }
+            else
+            {
                 ElementsLoc.Add(key, new List<Range>() { value });
             }
         }
 
+        /// <summary>
+        /// 获取选中位置的单词
+        /// </summary>
+        /// <param name="p">鼠标位置p</param>
+        /// <returns>当前位置最近的单词</returns>
+        private string GetPointedWord(Point p) {
+            string s = "";
+            var tr = Document.GetRangeFromPoint(p, PointOptions.ClientCoordinates);
+            int offsf = tr.StartPosition, offsb = tr.StartPosition;
+            Document.GetText(TextGetOptions.None, out s);
+            while (offsf <= s.Length - 1 && ((s[offsf] >= 'a' && s[offsf] <= 'z') || (s[offsf] >= 'A' && s[offsf] <= 'Z') || s[offsf].Equals('-')))
+                offsf++;
+            while (offsb >= 0 && ((s[offsb] >= 'a' && s[offsb] <= 'z') || (s[offsb] >= 'A' && s[offsb] <= 'Z') || s[offsb].Equals('-')))
+                offsb--;
+
+            try { s = s.Substring(offsb + 1, offsf - offsb - 1); }
+            catch (Exception) { s = ""; }
+
+            return s;
+        }
+
+        /// <summary>
+        /// 渲染词库中的单词项
+        /// </summary>
         private void RenderWord() {
 
         }
 
+        /// <summary>
+        /// 预渲染选中单词，TextPrerender = true
+        /// </summary>
+        private void Prerender() {
+            if (TextPrerender) {
+
+            }
+        }
+
         private string PrintDicItem(List<Range> ranges) {
-           // throw new NotImplementedException();
+            // throw new NotImplementedException();
             string s = "";
-            foreach (var r in ranges) {
+            foreach (var r in ranges)
+            {
                 s += "    " + r.ToString();
             }
             return s.ToString();
@@ -171,7 +237,5 @@ namespace ExReaderPlus.CustomeControls.NewControls {
             this.DefaultStyleKey = typeof(RichTextBox);
         }
         #endregion
-
     }
-
 }
